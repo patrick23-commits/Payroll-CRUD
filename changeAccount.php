6a1c685a2a48e23434e328547fdfe3d8b092ac77
Patrick Penaranda <patrickjabonillo.26@gmail.com>
2022-12-03 03:24:02 +0800

    Fix change account

--- changeAccount.php
+++ changeAccount.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 include_once("./payroll.php");
 if($_SERVER['REQUEST_METHOD'] == "POST")
 echo json_encode(["message"=>$payroll->changeAccount($_SESSION['username'], $_SESSION['status'])]);
